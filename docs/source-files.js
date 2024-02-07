c2428b875cc6abdf6c3ac8efaac42b848fd42c9f
arindas <arindas@users.noreply.github.com>
2024-02-07 18:14:48 +0000

    deploy: 6d9f568228091056c5d52ed5c459dc419df71df6

--- docs/source-files.js
+++
@@ -1,4 +0,0 @@
-var sourcesIndex = JSON.parse('{\
-"generational_cache":["",[["arena",[],["mod.rs"]],["cache",[],["lru_cache.rs","mod.rs"]],["collections",[],["list.rs","mod.rs"]],["map",[["impls",[],["alloc_btree_map.rs","mod.rs"]]],["mod.rs"]],["vector",[["impls",[],["alloc_vec.rs","array.rs","mod.rs"]]],["mod.rs"]]],["lib.rs"]]\
-}');
-createSourceSidebar();
